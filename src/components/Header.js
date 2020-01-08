c54260a1f543a31e5754d1428eb01782c5b9e171
daniel sc <su7cheng@gmail.com>
2020-01-08 18:30:44 -0200

    Add Header and Main components

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        const title = 'Neighborhood Map';
+        
+        return (
+            <React.Fragment>
+              <header>
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-sm-12 col-md-12 text-right">
+                      <h1>{title}</h1>
+                    </div>
+                  </div>
+                  <hr />
+                </div>
+              </header>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
