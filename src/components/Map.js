0547012acf01bcdff26fc18b4bcfd6f8a9477517
daniel sc <su7cheng@gmail.com>
2020-01-09 11:57:09 -0200

    Add async and defer attributes to string tag

--- src/components/Map.js
+++ src/components/Map.js
@@ -22,6 +22,8 @@ class Map extends Component {
             const mapScriptTag = document.createElement('script');
             mapScriptTag.type = 'text/javascript';
             mapScriptTag.src = `${URL}?key=${KEY}&v=${VERSION}`;
+            mapScriptTag.async = true;
+            mapScriptTag.defer = true;
             const firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(mapScriptTag, firstScriptTag);
 
