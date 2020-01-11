c5fd26fd6f085f601fb3b15c80fc46cdf92fe0cb
daniel sc <su7cheng@gmail.com>
2020-01-11 00:04:50 -0200

    Filter a list while typing the name of the place

--- src/components/PlacesList.js
+++ src/components/PlacesList.js
@@ -2,16 +2,47 @@ import React, { Component } from 'react';
 
 
 class PlacesList extends Component {
-    render() {
-        const listItem = this.props.places.map(place => {
-            return <li key={place.title}>{place.title}</li>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: '',
+            filteredPlaces: []
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.filterPlaces = this.filterPlaces.bind(this);
+    }
+    
+    handleInput(e) {
+        this.setState({
+            filter: e.target.value
         });
+    }
+
+    filterPlaces(filter) {
+        const self = this.props;
+        const filteredMarkers = [];
+        for(let i = 0; i < self.places.length; i++) {
+            if(self.places[i].title.toLowerCase().includes(filter.toLowerCase())) {
+                filteredMarkers.push(self.places[i]);
+            }
+        }
+        return filteredMarkers;
+    }
+
+    render() {
+        const filteredPlaces = this.filterPlaces(this.state.filter);
+        const listItems = filteredPlaces.map(item => {
+            return <li key={item.title}>{item.title}</li>;
+        });
+        const msg = <li key='message'>No Place Found</li>;
 
         return (
             <div>
-              <input  type="text" placeholder="Search" size="30" />
+              <input onChange={this.handleInput} type="text" placeholder="Search" size="30" />
               <ul>
-                {listItem}
+                { this.state.filter }
+                <br />
+                { listItems[0] !== undefined ? listItems : msg }
               </ul>
             </div>  
         );
