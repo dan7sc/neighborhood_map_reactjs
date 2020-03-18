b5960d64c042c2e8634a2257601670c7eeb3df12
dan7sc <su7cheng@gmail.com>
2020-03-18 12:40:43 -0300

    Filter places list

--- src/components/PlacesList2.jsx
+++ src/components/PlacesList2.jsx
@@ -2,67 +2,54 @@ import React, { Component } from 'react';
 import searchIcon from '../img/outline-search-24px.svg';
 import places from '../models/data';
 
+const PLACES = places;
+
 class PlacesList2 extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            placesList: places,
-            filteredMarkers: [],
-            filteredPlaces: []
+            filteredList: PLACES,
+            filter: ''
         };
     }
 
-    handleClick = (marker) => {
-        // const { map, infowindow } = { ...this.props };
-        // this.props.onShowInfoWindow(map, infowindow, marker);
-    }
-
     handleInput = (e) => {
-        // this.props.onHandleInput(e);
+        this.setState({
+            filter: e.target.value
+        });
     }
 
-    // filterMarkers = (items, filter) => {
-    //     const markers = items;
-    //     const filteredMarkers = [];
-    //     for(let i = 0; i < items.length; i++) {
-    //         if(items[i].title.toLowerCase().includes(filter.toLowerCase())) {
-    //             filteredMarkers.push(items[i]);
-    //             markers[i].setVisible(true);
-    //         }
-    //         else {
-    //             markers[i].setVisible(false);
-    //         }
-    //     }
-    //     return filteredMarkers;
-    // }
+    isFiltered = (filter, element) => {
+        return element.toLowerCase().includes(filter.toLowerCase());
+    }
 
-    // handleFilterMarkers = (markers, filter) => {
-    //     const filteredMarkers = filterMarkers(markers, filter);
-    //     this.setState({ filteredMarkers });
-    // }
+    handleFilterList = (toFilter, filter) => {
+        const filteredList = toFilter.filter(item => {
+            return this.isFiltered(filter, item.title);
+        });
+        return filteredList;
+    }
 
-    handleFilterPlaces = (places, filter) => {
-        const filteredPlaces = [];
-        const size = places.length;
-        for(let i = 0; i < size; i++) {
-            if(places[i].title.toLowerCase().includes(filter.toLowerCase())) {
-                filteredPlaces.push(places[i]);
-            }
+    componentDidMount = () => {
+        const filter = this.state.filter;
+        const filtered = this.handleFilterList(PLACES, filter);
+        this.setState({ filteredList: filtered });
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        const filter = this.state.filter;
+        if (filter !== '' && filter !== prevState.filter) {
+            const filtered = this.handleFilterList(PLACES, filter);
+            this.setState({ filteredList: filtered });
         }
-        this.setState({ filteredPlaces });
     }
 
     render() {
-        const filteredMarkers = this.state.filterMarkers;
-        const placesList = this.state.placesList;
-        const listFilteredMarkers = [];
-        // const listFilteredMarkers = filteredMarkers.map(item => {
-        //     return <li key={item.title} onClick={() => this.handleClick(item)}>{item.title}</li>;
-        // });
-        // const placesList = markers.map(item => {
-        //     return <li key={item.title} onClick={() => this.handleClick(item)}>{item.title}</li>;
-        // });
-        const listView = placesList.map(item => {
+        const { filteredList, filter } = { ...this.state };
+        const filteredListView = filteredList.map(item => {
+            return <li key={item.title}>{item.title}</li>;
+        });
+        const listView = PLACES.map(item => {
             return <li key={item.title}>{item.title}</li>;
         });
         const msg = <li key='message'>No place was found</li>;
@@ -74,10 +61,7 @@ class PlacesList2 extends Component {
                 <image xlinkHref={searchIcon} x="0" y="0" height="24px" width="24px"/>
               </svg>
               <ul className="list">
-                {/* { (placesList.length === 6 && listFilteredMarkers.length === 0 && filteredMarkers.length === 0) ? */}
-                {/*   placesList : (listFilteredMarkers[0] === undefined && listFilteredMarkers.length === 0) ? */}
-                {/*   msg : listFilteredMarkers } */}
-                { (placesList.length === 6) ? listView : msg }
+                { (filter === '' ) ? listView : filteredList !== '' && filteredList.length > 0 ? filteredListView : msg }
               </ul>
             </div>
         );
